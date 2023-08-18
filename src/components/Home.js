9072a363fe2cf633feab6c9e2226f8e2058c2b7a
Kishoreganesh Sundararajan <kishore7403@gmail.com>
2023-08-18 00:26:17 -0300

    home page added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,43 +1,57 @@
 import React, { useState, useEffect } from 'react';
+import { AppBar, Toolbar, Typography, Avatar, Button } from '@material-ui/core';
 
-function Home() {
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  avatar: {
+    marginLeft: '16px',
+  },
+  logoutButton: {
+    border: '1px solid white',
+    color: 'white',
+    marginLeft: '1rem',
+  },
+};
+
+function Navbar() {
   const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
   const [profilePicture, setProfilePicture] = useState("");
 
   useEffect(() => {
     // Fetching data from localStorage when the component mounts
     const storedName = localStorage.getItem("name");
-    const storedEmail = localStorage.getItem("email");
     const storedProfilePicture = localStorage.getItem("profilePicture");
 
     if (storedName) setName(storedName);
-    if (storedEmail) setEmail(storedEmail);
     if (storedProfilePicture) setProfilePicture(storedProfilePicture);
   }, []);
 
-  // Check if the profile picture is a URL or base64
-  const isUrl = profilePicture && profilePicture.startsWith("http");
+  const handleLogout = () => {
+    // Perform logout actions here
+    // For example, clearing localStorage, redirecting, etc.
+  };
 
   return (
-    <div>
-      <h2>Profile Details</h2>
-      <div>
-        <strong>Name:</strong> {name}
-      </div>
-      <div>
-        <strong>Email:</strong> {email}
-      </div>
-      <div>
-        <strong>Profile Picture:</strong>
-        {isUrl ? (
-          <img src={profilePicture} alt="Profile" style={{ width: '100px', height: '100px' }}/>
-        ) : profilePicture && (
-          <img src={`data:image/jpeg;base64,${profilePicture}`} alt="Profile" style={{ width: '100px', height: '100px' }}/>
-        )}
-      </div>
+    <div style={styles.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={styles.title}>
+            ChatRooms
+          </Typography>
+          <Typography>{name}</Typography>
+          {profilePicture && <Avatar alt="Profile" src={profilePicture} style={styles.avatar} />}
+          <Button style={styles.logoutButton} onClick={handleLogout}>
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
 
-export default Home;
+export default Navbar;
